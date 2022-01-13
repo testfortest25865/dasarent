e70ccd514238b8f4da919472bd730e1ba0a941fb
testfortest25865 <97685031+testfortest25865@users.noreply.github.com>
2022-01-13 21:48:15 +0500

    Add files via upload

---
+++ js/reviews.js
@@ -0,0 +1,2 @@
+"use strict";(function(){new Swiper(".review__slider",{slidesPerView:1,loop:!0,// spaceBetween: 30,
+navigation:{nextEl:".review__slider-button_next",prevEl:".review__slider-button_prev"},grabCursor:!0,breakpoints:{888:{slidesPerView:3}}})})();
